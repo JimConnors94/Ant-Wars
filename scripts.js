2f49e8f82cb08a17edd4153d315dd9844d9bf9e5
JimCOnnors94 <194996183+JimConnors94@users.noreply.github.com>
2025-02-03 19:28:07 -0330

    Updated code from session

--- scripts.js
+++ scripts.js
@@ -1,281 +1,266 @@
-
-
 const defaultGameState = {
-    lastRaidTimer: 0,
-    lastFoodCostTimer: 0,
-    gameTimer : 0,
+  lastRaidTimer: 0,
+  lastFoodCostTimer: 0,
+  gameTimer: 0,
 
-    playerAntHill: {
-      resources: {
-        eggs: 0,
-        food: 100,
-      },
-      ants: {
-        workers: 10,
-        warriors: 0,
-        guards: 0,
-
+  playerAntHill: {
+    resources: {
+      eggs: 0,
+      food: 100,
     },
-    enemyAntHill: {
-      ants: {
-        enemy: 10,
+    ants: {
+      workers: 10,
+      warriors: 0,
+      guards: 0,
+    },
+  },
+  enemyAntHill: {
+    ants: {
+      enemy: 10,
     },
     battleLog: [],
     enemyKilled: 0,
     highestPopulation: 0,
+  },
+};
 
-    };
-  
+let gameState = {};
 
-  
-  let gameState = {};
-  
-  function updatePlayerStats() {
+function updatePlayerStats() {
+  document.getElementById("Eggs").textContent =
+    gameState.playerAntHill.resources.eggs;
 
-    document.getElementById("Eggs").textContent =
-      gameState.playerAntHill.resources.eggs;
-    
-    document.getElementById("foodCount").textContent =
-      gameState.playerAntHill.resources.food;
-   
-    document.getElementById("workerCount").textContent =
-      gameState.playerAntHill.ants.workers;
+  document.getElementById("foodCount").textContent =
+    gameState.playerAntHill.resources.food;
 
-    document.getElementById("warriorCount").textContent =
-      gameState.playerAntHill.ants.warriors;
+  document.getElementById("workerCount").textContent =
+    gameState.playerAntHill.ants.workers;
 
-    document.getElementById("guardCount").textContent =
-      gameState.playerAntHill.ants.guards;
+  document.getElementById("warriorCount").textContent =
+    gameState.playerAntHill.ants.warriors;
 
-  }
-  
-  function initializeGameState() {
-    const savedState = localStorage.getItem("antWarsGameState");
-
-  
-    if (savedState) {
-      
-      gameState = JSON.parse(savedState);
-    } else {
-      
-      gameState = JSON.parse(JSON.stringify(defaultGameState));
-    }
-  
-    updatePlayerStats();
-  
-    console.log(savedState ? "Loaded saved game." : "Starting a new game.");
-  }
-  
-  function startNewGame() {
-    
-    localStorage.setItem("antWarsGameState", JSON.stringify(defaultGameState));
-  
-    updatePlayerStats();
-  
-    console.log("New game started.");
-  }
-  
-  function saveGame() {
-    localStorage.setItem("antWarsGameState", JSON.stringify(gameState));
-  
-    console.log("Game saved.");
-  }
-  
-  function loadGame() {
-    const savedState = JSON.parse(localStorage.getItem("antWarsGameState"));
-  
-    if (savedState) {
-      gameState = savedState;
-      console.log("Game loaded.");
-      updatePlayerStats();
-    } else {
-      console.log("No saved game found.");
-    }
-  }
-  
-  function nameOfFunction() {
-    console.log("Hey this is your new function oh yeah");
-  }
-  
-  document.addEventListener("DOMContentLoaded", initializeGameState);
-
-    }
-
- function Hatchant() {
-      
-        case 'Hatch Worker':
-
-      if (player.eggs <= 0) {
-        break;} 
-        
-        else {
-            guards += 1;
-            eggs -= 1;
-        break;}
-        
-        case 'Hatch Warrior':
-
-      if (player.eggs <= 0) {
-        break;} 
-
-        else {
-            warriors += 1;
-            eggs -= 1;
-        break;}
-
-        case 'Hatch Guard':
-
-      if (player.eggs <= 0) {
-        break;} 
-        
-        else {
-            workers += 1;
-            eggs -= 1;
-        break;}
-
-    }
-
- function raidTimer () 
-    setInterval() {
-    if (Math.random() < 0.5) {
-
-        const enemyAttack = enemyants * 5
-
-        playerDefence = guards * 16 + warriors * 8 + workers * 1
-
-        const loss = Math.round(10 * enemyAttack / playerDefence);
-
-            let antsToKill = loss;, 
-
-            while (loss > 0)
-
-                if (guards > 0 & warriors > 0) {
-    
-                    if (Math.random() < 0.5) {
-
-                        guards -= 1;
-                        loss -= 1; 
-    
-                    } else {
-                        warriors -= 1;
-                        loss -= 8; 
-                    }
-    
-                } else if (guards > 0) {
-    
-                    guards -= 1;
-                    loss -= 8; 
-    
-                } else if (warriors > 0) {
-    
-                    warriors -= 1;
-                    loss -= 8; 
-    
-                } else if (workers > 0) {
-                    workers -= 1;
-                    loss -= 1; 
-    
-                } else {
-                    break; 
-                }
-            }
-
-        }
-    
-        updatePlayerStats();
-    }, 30000);
-
-
-            
-        
- }
-
-
- function foodCostTimer (){
-
-    setInterval() {
-
-        const foodCost = workers * 1 + guards * 8 + warriors * 8;) 
-
-        food -= Math.floor(foodCost);
-
-        if food < 0
-
-        let foodDeficit = food
-
-        while (foodDeficit > 0)
-
-            if (guards > 0 & warriors > 0) {
-
-                if (Math.random() < 0.5) {
-
-                    guards -= 1;
-                    foodDeficit -= 8; 
-
-                } else {
-                    warriors -= 1;
-                    foodDeficit -= 8; 
-                }
-
-            } else if (guards > 0) {
-
-                guards -= 1;
-                foodDeficit -= 8; 
-
-            } else if (warriors > 0) {
-
-                warriors -= 1;
-                foodDeficit -= 8; 
-
-            } else if (workers > 0) {
-                workers -= 1;
-                foodDeficit -= 1; 
-
-            } else {
-                break; 
-            }
-        }
-        gameState.playerAntHill.resources.food = 0; 
-    }
-
-    updatePlayerStats();
-}, 10000);
-
-
-
-
-
-
-
-    }
- }
-
-function foodGathheringTimer  {
-
-    setInterval(){
-        
-        const foodGathered = workers * 1 + guards * 0.1 warriors * 0.1;)
-
-        food += Math.floor(foodGathered);
-
-        updatePlayerStats();
-
-        1000);
-    }
-
+  document.getElementById("guardCount").textContent =
+    gameState.playerAntHill.ants.guards;
 }
 
-function Layegg  {
+function initializeGameState() {
+  const savedState = localStorage.getItem("antWarsGameState");
 
-    setInterval(gameState.playerAntHill.resources.eggs++; 1000)
+  if (savedState) {
+    gameState = JSON.parse(savedState);
+  } else {
+    gameState = JSON.parse(JSON.stringify(defaultGameState));
+  }
 
+  updatePlayerStats();
 
+  console.log(savedState ? "Loaded saved game." : "Starting a new game.");
 }
-    
 
-        
+function startNewGame() {
+  localStorage.setItem("antWarsGameState", JSON.stringify(defaultGameState));
 
+  updatePlayerStats();
 
+  console.log("New game started.");
+}
+
+function saveGame() {
+  localStorage.setItem("antWarsGameState", JSON.stringify(gameState));
+
+  console.log("Game saved.");
+}
+
+function loadGame() {
+  const savedState = JSON.parse(localStorage.getItem("antWarsGameState"));
+
+  if (savedState) {
+    gameState = savedState;
+    console.log("Game loaded.");
+    updatePlayerStats();
+  } else {
+    console.log("No saved game found.");
+  }
+}
+
+function nameOfFunction() {
+  console.log("Hey this is your new function oh yeah");
+}
+
+function startTimer() {
+  setInterval(() => {
+    gameState.gameTimer += 1;
+
+    document.getElementById("gameTimer").innerText = gameState.gameTimer;
+  }, 1000);
+
+  console.log("Timer Started");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeGameState();
+  startGameTimer();
+});
+
+//  function Hatchant() {
+
+//         case 'Hatch Worker':
+
+//       if (player.eggs <= 0) {
+//         break;}
+
+//         else {
+//             guards += 1;
+//             eggs -= 1;
+//         break;}
+
+//         case 'Hatch Warrior':
+
+//       if (player.eggs <= 0) {
+//         break;}
+
+//         else {
+//             warriors += 1;
+//             eggs -= 1;
+//         break;}
+
+//         case 'Hatch Guard':
+
+//       if (player.eggs <= 0) {
+//         break;}
+
+//         else {
+//             workers += 1;
+//             eggs -= 1;
+//         break;}
+
+//     }
+
+//  function raidTimer ()
+//     setInterval() {
+//     if (Math.random() < 0.5) {
+
+//         const enemyAttack = enemyants * 5
+
+//         playerDefence = guards * 16 + warriors * 8 + workers * 1
+
+//         const loss = Math.round(10 * enemyAttack / playerDefence);
+
+//             let antsToKill = loss;,
+
+//             while (loss > 0)
+
+//                 if (guards > 0 & warriors > 0) {
+
+//                     if (Math.random() < 0.5) {
+
+//                         guards -= 1;
+//                         loss -= 1;
+
+//                     } else {
+//                         warriors -= 1;
+//                         loss -= 8;
+//                     }
+
+//                 } else if (guards > 0) {
+
+//                     guards -= 1;
+//                     loss -= 8;
+
+//                 } else if (warriors > 0) {
+
+//                     warriors -= 1;
+//                     loss -= 8;
+
+//                 } else if (workers > 0) {
+//                     workers -= 1;
+//                     loss -= 1;
+
+//                 } else {
+//                     break;
+//                 }
+//             }
+
+//         }
+
+//         updatePlayerStats();
+//     }, 30000);
+
+//  }
+
+//  function foodCostTimer (){
+
+//     setInterval() {
+
+//         const foodCost = workers * 1 + guards * 8 + warriors * 8;)
+
+//         food -= Math.floor(foodCost);
+
+//         if food < 0
+
+//         let foodDeficit = food
+
+//         while (foodDeficit > 0)
+
+//             if (guards > 0 & warriors > 0) {
+
+//                 if (Math.random() < 0.5) {
+
+//                     guards -= 1;
+//                     foodDeficit -= 8;
+
+//                 } else {
+//                     warriors -= 1;
+//                     foodDeficit -= 8;
+//                 }
+
+//             } else if (guards > 0) {
+
+//                 guards -= 1;
+//                 foodDeficit -= 8;
+
+//             } else if (warriors > 0) {
+
+//                 warriors -= 1;
+//                 foodDeficit -= 8;
+
+//             } else if (workers > 0) {
+//                 workers -= 1;
+//                 foodDeficit -= 1;
+
+//             } else {
+//                 break;
+//             }
+//         }
+//         gameState.playerAntHill.resources.food = 0;
+//     }
+
+//     updatePlayerStats();
+// }, 10000);
+
+//     }
+//  }
+
+// function foodGathheringTimer  {
+
+//     setInterval(){
+
+//         const foodGathered = workers * 1 + guards * 0.1 warriors * 0.1;)
+
+//         food += Math.floor(foodGathered);
+
+//         updatePlayerStats();
+
+//         1000);
+//     }
+
+// }
+
+// function Layegg  {
+
+//     setInterval(gameState.playerAntHill.resources.eggs++; 1000)
+
+// }
 
 /* 
 
@@ -439,4 +424,3 @@ function Layegg  {
         or
 
         Your population reaching zero, due to either food shortage or enemy raid will result in defeat. */
-    }
